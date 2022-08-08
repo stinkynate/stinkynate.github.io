a5dfaec60bfa5e8a5c9170a76f16c1c67e39d9bc
stinkynate <nathan.luttmann@gmail.com>
2022-08-08 08:38:18 -0500

    Test UTF-8

--- js/events.js
+++ js/events.js
@@ -1,6 +1,6 @@
 Papa.parse("https://stinkynate.github.io/events.csv", {
 	worker: true,
-	header: true,
+	//header: true,
 	download: true,
 	complete: function(results) {
 		console.log(results);
@@ -9,12 +9,19 @@ Papa.parse("https://stinkynate.github.io/events.csv", {
 		//d3.selectAll("p").classed('noresults', true).html("")
 		//d3.event.preventDefault();
 		
-		for (var i = 0; i < results.data.length; i++) {
+		for (var i = 3; i < results.data.length; i++) {
 		   //console.log(results.data[5][3]);
 		  // console.log(output[i]['avg_vote'])
 		  // d3.select("tbody>tr>td").text(output[i]['original_title']);
-		  d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>"+"<a href=https://www.imdb.com/title/"+results.data[i]["imdb_title_id"]+" target='_blank'>"+(results.data[i]["original_title"])+"</a>"
-		  + "</td>");
+		  d3.select("tbody").insert("tr").html(
+		  "<td>"+(i-2)+"</td>"+
+		  "<td>"+results.data[i][1]+"</td>"+ // Tag
+		  "<td>"+results.data[i][2]+ "</td>"+ // Event
+		  "<td><img class='pokemon' src='https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/"+results.data[i][4].toLowerCase()+".png' inline=false/>"+results.data[i][4]+ "</td>"+ // Pokemon
+		  "<td>"+results.data[i][6]+ "</td>"+ // Shiny
+		  "<td><img class='ball' src='https://raw.githubusercontent.com/msikma/pokesprite/master/icons/ball/cherish.png'/>"+results.data[i][8]+"</td>"+ // Ball
+		  "<td>"+results.data[i][9]+ "</td>"+ // Level
+		  "<td>"+results.data[i][10]+ "</td>"); // Gender
 		}
 	}
 });
