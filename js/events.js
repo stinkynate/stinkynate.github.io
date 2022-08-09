ec3bcae11ca5011a5d876a487a8593bc4f73e46c
stinkynate <nathan.luttmann@gmail.com>
2022-08-08 22:44:22 -0500

    Add sprite hint text for broken images

--- js/events.js
+++ js/events.js
@@ -26,7 +26,7 @@ function parseEvents()
 			//d3.event.preventDefault();
 			for (var i = 3; i < results.data.length; i++) {
 			  var row = results.data[i];
-			  if (row.length <= 1 || row[4] == "" || row[37] == "TRUE")
+			  if (row.length <= 1 || row[4] == "" || row[38] == "TRUE")
 				  continue;
 
 			  var rowHtml = "<td>"+(i-2)+"</td>";
@@ -56,41 +56,42 @@ function addEvent(row)
 }
 function addPokemon(row, i)
 {
-	var shinyLink = row[5] == "" ? "normal" : "shiny";
-	return "<td><div><img class='pokemon' data-pokemon='"+row[4].toLowerCase()+"' data-shiny='"+row[5]+"' src='https://projectpokemon.org/images/sprites-models/swsh-"+shinyLink+"-sprites/"+row[4].toLowerCase()+".gif' onload='checkImageSize(this)' onerror='loadGen7Animated(this)' height='40'/></div>"+row[4]+ "</td>";
-	//https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/
-	//https://projectpokemon.org/images/sprites-models/swsh-normal-sprites/grookey.gif
+	var shinyLink = row[6] == "" ? "normal" : "shiny";
+	var pokemon = row[4].toLowerCase();
+	if (row[5] <> "")
+		pokemon = row[5].toLowerCase();
+	return "<td><div><img class='pokemon' data-pokemon='"+pokemon+"' data-shiny='"+row[6]+"' src='https://projectpokemon.org/images/sprites-models/swsh-"+shinyLink+"-sprites/"+pokemon+".gif' onload='checkImageSize(this)' onerror='loadGen7Animated(this)' height='40'/></div>"+row[4]+ "</td>";
 }
 function addShiny(row)
 {
-	return "<td>"+row[5]+ "</td>";
+	return "<td>"+row[6]+ "</td>";
 }
 function addBall(row)
 {
-	var ball = row[6] == "" ? "" : ballMap.get(row[6]);
+	var ball = row[7] == "" ? "" : ballMap.get(row[6]);
 	if (ball == undefined)
-		console.log(row[6]);
+		console.log(row[7]);
 	return "<td><img class='ball' src='"+ball+"' alt='"+row[6]+"' title='"+row[6]+"'/></td>";
 }
 function addLevel(row)
 {
-	return "<td>"+row[7]+ "</td>";
+	return "<td>"+row[8]+ "</td>";
 }
 function addGender(row)
 {
-	return "<td>"+row[8]+ "</td>";
+	return "<td>"+row[9]+ "</td>";
 }
 function addOT(row)
 {
-	return "<td>"+row[23]+ "</td>";
+	return "<td>"+row[24]+ "</td>";
 }
 function addTID(row)
 {
-	return "<td>"+row[24]+ "</td>";
+	return "<td>"+row[25]+ "</td>";
 }
 function addHistory(row)
 {
-	return "<td>"+row[29]+ "</td>";
+	return "<td>"+row[30]+ "</td>";
 }
 
 function checkImageSize(img)
